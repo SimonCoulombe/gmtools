d3b22a3330679e8965e89221416b469998e11bb8
George Milsom <georgemilsom@hotmail.com>
2018-03-30 10:00:48 +0100

    added null xgb_autotune tests

---
+++ tests/testthat/test_xgb_autotune.R
@@ -0,0 +1,9 @@
+## Set the test context
+   context('Check the xgb_autotune works properly')
+
+## Load data for tests
+   data(dataCar,package = 'insuranceData')
+
+## Begin the testing
+
+   ## TODO: I need to think of a good way to test this!
